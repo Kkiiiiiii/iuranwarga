ff68d02e8c197a7a7fa3b9d434679d20356ebbc6
Kkiiiiiii <rikizhakaria@gmail.com>
2025-08-08 09:44:39 +0700

    Controller

--- resources/views/admin/dues_category/dues_category.blade.php
+++ resources/views/admin/dues_category/dues_category.blade.php
@@ -1,4 +1,71 @@
 @extends('admin.layout')
 @section('content')
+<div class="container">
+    <a href="{{ route('admin.wargaCreate') }}" class="btn btn-sm btn-info mt-5 align-items-end">Tambah Data Warga</a>
+    <p>Data Petugas</p>
+    <table class="table table-striped table-hover">
+        <thead class="table">
+            <tr>
+                <th>No</th>
+                <th>Nama</th>
+                <th>Username</th>
+                <th>Nohp</th>
+                <th>Address</th>
+                <th>Petugas</th>
+                <th>Nominal</th>
+                <th>Status</th>
+                <th>Aksi</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td class="text-success"></td>
+            <td class="text-primary"></td>
+            <td>
+                <a href="" class="btn btn-sm btn-warning">Edit</a>
+                <a href="" class="btn btn-sm btn-danger" onclick="return confirm('Yakin data warga ( ini dihapus?)')">Delete</a>
+            </td>
+        </tr>
+    </tbody>
+    </table>
+    <p>Data Warga</p>
+    <table class="table table-striped table-hover">
+        <thead class="table">
+            <tr>
+                <th>No</th>
+                <th>Nama</th>
+                <th>Username</th>
+                <th>Nohp</th>
+                <th>Address</th>
+                <th>Petugas</th>
+                <th>Nominal</th>
+                <th>Status</th>
+                <th>Aksi</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td class="text-success"></td>
+            <td class="text-primary"></td>
+            <td>
+                <a href="" class="btn btn-sm btn-warning">Edit</a>
+                <a href="" class="btn btn-sm btn-danger" onclick="return confirm('Yakin data warga (ini dihapus?)')">Delete</a>
+            </td>
+        </tr>
+        </tbody>
+    </table>
+</div>
 
 @endsection
--- resources/views/admin/folder_user/tambah_users.blade.php
+++ resources/views/admin/folder_user/tambah_users.blade.php
@@ -60,7 +60,7 @@
             <label for="level" class="form-label">Level Akses</label>
             <div class="input-group">
                 <select class="form-select" name="level" id="level">
-                    <option value="admin">Admin</option>
+                    <option value="admin">officer</option>
                     <option value="warga">Warga</option>
                 </select>
                 <span class="input-group-text"><i class="fa-solid fa-database"></i></span>
