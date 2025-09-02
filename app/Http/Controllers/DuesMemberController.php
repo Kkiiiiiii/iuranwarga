5fb5b3405208fa911aa5ba208dd0a954ebed222a
unknown <hilmanagustian2408@gmail.com>
2025-09-02 09:26:25 +0700

    MemberOfficer

--- app/Http/Controllers/DuesMemberController.php
+++ app/Http/Controllers/DuesMemberController.php
@@ -7,6 +7,7 @@ use App\Models\DuesMembers;
 use App\Models\User;
 use Illuminate\Contracts\Encryption\DecryptException;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Crypt;
 
 class DuesMemberController extends Controller
@@ -16,14 +17,22 @@ class DuesMemberController extends Controller
         $data['duesMember'] = DuesMembers::all();
         $data['Warga'] = User::all();
         $data['Category'] = DuesCategory::all();
-        return view('admin.member.dues_member', $data);
+        if (Auth::user()->level == 'admin') {
+            return view('admin.member.dues_member', $data);
+        }else if (Auth::user()->level == 'officer') {
+            return view('officer.member.dues_member', $data);
+        }
     }
 
     public function create()
     {
        $data['Warga'] = User::all();
        $data['Category'] = DuesCategory::all();
-       return view("admin.member.tambah_duesMember", $data);
+       if (Auth::user()->level == 'admin') {
+            return view("admin.member.tambah_duesMember", $data);
+        }else if (Auth::user()->level == 'officer') {
+            return view("officer.member.tambah_duesMember", $data);
+        }
     }
 
     public function store(Request $request)
@@ -34,7 +43,11 @@ class DuesMemberController extends Controller
         ]);
 
         DuesMembers::create($validation);
-        return redirect()->route('admin.dues_member')->with('success', 'Berhasil menambah data Member');
+        if (Auth::user()->level == 'admin') {
+            return redirect()->route('admin.dues_member')->with('success', 'Berhasil menambah data Member');
+        }else if (Auth::user()->level == 'officer') {
+            return redirect()->route('officer.dues_member')->with('success', 'Berhasil menambah data Member');
+        }
     }
 
     public function edit(String $id){
@@ -47,7 +60,11 @@ class DuesMemberController extends Controller
         $data['Member'] = DuesMembers::find($id);
         $data['Warga'] = User::all();
         $data['Category'] = DuesCategory::all();
-        return view('admin.member.edit_duesMember', $data);
+        if (Auth::user()->level == 'admin') {
+            return view('admin.member.edit_duesMember', $data);
+        }else if (Auth::user()->level == 'officer') {
+            return view('officer.member.edit_duesMember', $data);
+        }
     }
 
     public function update(Request $request, String $id){
@@ -60,10 +77,15 @@ class DuesMemberController extends Controller
         $validation = $request->validate([
             'dues_categories_id' => 'required',
         ]);
-        
+
         $member = DuesMembers::find($id);
         $member->update($validation);
-        return redirect(route('admin.dues_member'))->with('success', 'Data berhasil diubah');
+
+        if (Auth::user()->level == 'admin') {
+            return redirect(route('admin.dues_member'))->with('success', 'Data berhasil diubah');
+        }else if (Auth::user()->level == 'officer') {
+            return redirect(route('officer.dues_member'))->with('success', 'Data berhasil diubah');
+        }
     }
 
     public function delete(String $id)
@@ -76,7 +98,11 @@ class DuesMemberController extends Controller
 
         $Member = DuesMembers::find($id);
         $Member->delete();
-        
-        return redirect(route('admin.dues_member'))->with('success', 'Data berhasil dihapus');
+
+        if (Auth::user()->level == 'admin') {
+            return redirect(route('admin.dues_member'))->with('success', 'Data berhasil dihapus');
+        }else if (Auth::user()->level == 'officer') {
+            return redirect(route('officer.dues_member'))->with('success', 'Data berhasil dihapus');
+        }
     }
 }
